474ad939cb3d9e6927056589d26ece699249789a
BharaniHK <bharanihk4@gmail.com>
2025-05-30 11:51:21 +0530

    Create five.jsx

---
+++ comp/five.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export Five =()=>{
+
+  return(<>
+    <p>Hello worldd/p>
+    <p id="id1">Five component ID</p>
+      <p name="name1">Five component NAME</p>
+  </>)
+}
