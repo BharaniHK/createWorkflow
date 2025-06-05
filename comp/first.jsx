4783fdc81e40114d63e87d9a1702f62e7fbfc999
BharaniHK <bharanihk4@gmail.com>
2025-06-05 16:55:00 +0530

    Update first.jsx

--- comp/first.jsx
+++ comp/first.jsx
@@ -9,7 +9,7 @@ function App() {
        <p name ="Firstname">First1</p>
        <p id="first_id1">First</p>
       <form>   
-        <input name="username123" id="uid1" value="abcd"></input>
+        <input name ="username123" id="uid1" value="abcd"></input>
         <button name ="Submit">Submit</button>
       </form>
     </div>  
