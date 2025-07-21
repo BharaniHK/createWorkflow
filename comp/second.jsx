c8a8ff0d09740530f0685bf3d64096fe582d7444
BharaniHK <bharanihk4@gmail.com>
2025-07-21 11:36:42 +0530

    Update second.jsx

--- comp/second.jsx
+++ comp/second.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Second=()=>{
   return(
     <div> 
-    <label id="second">Second</label> 
-     < p name = "second_name">Second</p>
-     <p name="second3">Second123</p>
+    <label id="second2">Second</label> 
+     < p name = "second_name1">Second</p>
+     <p name= "second3">Second123</p>
      <p name ="third">Second</p>
     </div>
   )
