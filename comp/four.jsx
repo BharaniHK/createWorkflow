a3a9321362c58b035c451b963bf0650c937638bd
BharaniHK <bharanihk4@gmail.com>
2025-07-22 11:37:31 +0530

    Update four.jsx

--- comp/four.jsx
+++ comp/four.jsx
@@ -4,7 +4,7 @@ export const Four=()=>{
   return( j
     <div>
     <label id="four">Second</label>
-    <label name="Four1">Second</label>
+    <label name="Four2">Second</label>
     <span>abc</span>
     <p name= "four">Second</p> 
     <label id= "four3">Second</label>
