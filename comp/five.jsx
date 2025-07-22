13594393c723c1de0c5faf869777e639ed1d86ab
BharaniHK <bharanihk4@gmail.com>
2025-07-22 11:54:07 +0530

    Update five.jsx

--- comp/five.jsx
+++ comp/five.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export Five =()=>{
 
   return(<>
-    <span name="newValue1">Five component NAME</span>
-    <p name="Oldvalue00">Hello world123</p>
+    <span name="second_name">Five component NAME</span>
+    <p name="Oldvalue00">Hello world</p>
     <p id="id1">Five component ID</p>
-    <span name="name0">five component NAME</span>
+    <span name="name1">five component NAME</span>
     <p name="name2">Five component NAME</p>
     <p name="name89">Five component NAME</p>
   </>)
