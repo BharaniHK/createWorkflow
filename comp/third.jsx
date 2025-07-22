93addca7abd75b43004f0d9db77909e9f561e8d8
BharaniHK <bharanihk4@gmail.com>
2025-07-22 12:23:10 +0530

    Update third.jsx

--- comp/third.jsx
+++ comp/third.jsx
@@ -8,12 +8,12 @@ function Third() {
   return( 
     <div>
       <p id="third">Third</p>
-      <span name="third">Third</span>
+      <span name="third1">Third</span>
        <p name123="third">Third</p>
        <p abcname="third">Third</p
       <p name1="third">Third</p>
       <p dname="third12">Third123</p>
-       <div name="third_div">Division Third</div>
+       <div name="third_div2">Division Third</div>
        <div name="inner_div">Division Third2</div>
       </div>  
   )
