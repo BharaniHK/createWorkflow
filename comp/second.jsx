4c01c7d0af316579992a831e4137574cdaa71ad5
BharaniHK <bharanihk4@gmail.com>
2025-07-23 16:18:35 +0530

    Update second.jsx

--- comp/second.jsx
+++ comp/second.jsx
@@ -4,8 +4,8 @@ export const Second=()=>{
   return(
     <div> 
     <label id="second">Second</label> 
-     < p name="value1">Second</p>
-     <p name="value2">Second123</p>
+     < p name="newValue_1">Second</p>
+     <p name="newValue_2">Second123</p>
      <p name ="third">Second</p>
     </div>
   )
