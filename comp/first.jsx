dea8dd38eb6a36e9b64347026813402fd24ad388
BharaniHK <bharanihk4@gmail.com>
2025-07-23 14:54:10 +0530

    Update first.jsx

--- comp/first.jsx
+++ comp/first.jsx
@@ -7,7 +7,7 @@ function App() {
   return(
     <div> 12345
       <p name="new">First1</p>  
-       <p id="first_id">First</p>
+       <p id="firs;t_id">First</p>
       <form>  
         <input name="input_name1" id="uid1" value="abcd"></input>
         <input  id="uid2" value="abcd"></input>
