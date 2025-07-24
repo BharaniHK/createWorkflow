593308ed3895a05362779a56f2f764e96e8e469a
BharaniHK <bharanihk4@gmail.com>
2025-07-24 11:48:26 +0530

    Update five.jsx

--- comp/five.jsx
+++ comp/five.jsx
@@ -8,6 +8,7 @@ export Five =()=>{
     <p id="id2">Five component ID</p>
     <p id="id3">Five component ID</p>
       <p id="id4">Five component ID</p>
+     <p id="id5">Five component ID</p>
     <span name="new_updated_name">five component NAME</span>
     <p name="dev_name2">Five component NAME</p>
     <p name="name9">Five component NAME</p>
