0d1f90877add6094aedd3d9f2d93db4c632c8207
BharaniHK <bharanihk4@gmail.com>
2025-09-30 15:37:50 +0530

    Update first.jsx

--- comp/first.jsx
+++ comp/first.jsx
@@ -7,10 +7,10 @@ function App() {
   return(
     <div> 12345
       <p name="new">First1</p>   
-       <p id="first_id">First</p>
+       <p id="second_id">First</p>
       <form>  
         <input name="input_name1" id="uid1" value="abcd"></input>
-        <input  id="uid2" value="abcd"></input>
+        <input  id="updatedid4" value="abcd"></input>
         <p name="Submit">Submit</p>
       </form>
     </div>  
